7c0020f15fa582814125e5eba96fee1f9463b304
Ricardo Valfreixo <valfreixo@gmail.com>
2009-11-07 18:36:53 +0000

    global update

---
+++ index.php
@@ -0,0 +1,12 @@
+<pre>
+<?php
+
+	require('system/classes/application.class.php');
+
+	$app = new Application;
+
+	
+	
+	var_dump($_SERVER);
+?>
+</pre>
---
+++ system/classes/routes.class.php
@@ -0,0 +1,23 @@
+<?php
+
+
+	#doc
+	#	classname:	Router
+	#	scope:		PUBLIC
+	#
+	#/doc
+	
+	class Router extends AnotherClass
+	{
+		#	internal variables
+		
+		#	Constructor
+		function __construct (argument)
+		{
+			# code...
+			
+		}
+		###	
+	
+	}
+	###
